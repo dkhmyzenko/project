853ee46109eacc62f5d0e42d7a955551f7ae9789
Denis <dendyzet@gmail.com>
2022-05-06 15:41:21 +0300

    Cicles and condition

--- 15task/015script.js
+++ 15task/015script.js
@@ -1,4 +1,3 @@
-
 /* Задание на урок:
 
 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
@@ -12,38 +11,7 @@
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-for(let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов?',''),
-    b = prompt('На сколько оцените его?','');
-     
-    if(a != null && b != null && a != '' && b != '' && a.length <=50) {
-        personalMovieDB.movies[a] = b;  
-    } else {
-       i--;
-    }
-}
-
-if(personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if(personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-console.log("Произошла ошибка");
-}
-console.log(personalMovieDB);
-
+// Код возьмите из предыдущего домашнего задания
